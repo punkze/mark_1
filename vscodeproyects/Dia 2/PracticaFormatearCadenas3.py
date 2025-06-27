e6548da03e5bdeb84a584f2253b809b823369a0a
Leo Workstation <ponceventura@google.com>
2025-06-27 01:21:00 +0000

    Last push from format

--- vscodeproyects/Dia 2/PracticaFormatearCadenas3.py
+++ vscodeproyects/Dia 2/PracticaFormatearCadenas3.py
@@ -1,3 +1,3 @@
 puntos_anteriores = 875
 puntos_nuevos = 350
-print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores + puntos_nuevos} puntos")
---
+++ vscodeproyects/Dia 2/operadores.py
@@ -0,0 +1,14 @@
+x = 6
+y = 2
+z = 7
+
+print(f"{x} mas {y} es igual a {x + y}")
+print(f"{x} menos {y} es igual a {x - y}")
+print(f"{x} por {y} es igual a {x * y}")
+print(f"{x} dividido {y} es igual a {x / y}")
+
+print(f"{z} dividido al piso de {y} es igual a {z // y}")
+print(f"{z} modulo de {y} es igual a {z % y}")
+print(f"{x} elevado a la {y} es igual a {x ** y}")
+print(f"elevado a la {3} es igual a {x ** 3}")
+print(f"La raiz cuadrada de {x} es igual a {x ** 0.5}")
